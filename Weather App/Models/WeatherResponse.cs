f072352d4becc440d2934da40168f0f94b1cca30
Kacper Kuszek <kuszekkacper@gmail.com>
2026-07-20 21:41:58 +0200

    Mvvm structure created and models

---
+++ Weather App/Models/WeatherResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Weather_App.Models
+{
+    public class WeatherResponse
+    {
+        public string Name { get; set; }
+        public MainData Main { get; set; }
+        public WindData Wind { get; set; }
+        public SysData Sys { get; set; }
+
+    }
+}
---
+++ Weather App/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_App.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
